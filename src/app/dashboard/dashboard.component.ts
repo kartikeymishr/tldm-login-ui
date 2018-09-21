3dd75d2153daf97600b3a830b184c225cb12336c
Kartikey <kmishr11@in.ibm.com>
2018-09-21 15:34:44 +0530

    fixed issues v3

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -19,6 +19,7 @@ export class DashboardComponent implements OnInit {
     }
 
     logout() {
+        this.messageService.messagesArr = [];
         this.router.navigateByUrl('/login');
         this.messageService.disconnect();
     }
